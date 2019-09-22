51633c252bd088dd92fe14eb8eec52a88a52b6d7
York <york.tang@icloud.com>
2019-09-22 18:26:00 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -404,8 +404,9 @@ config_ur_args(){
 }
 
 # 安装Docker
-if [ -z $(docker -v) ]; then
-	courl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+INFO=`docker -v`
+if [ -z ${INFO} ]; then
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 	apt update && apt install -y docker-ce jq
 	systemctl enable docker.service
